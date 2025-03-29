00e744db0273fa28775ea3fb3da858b0e8a05992
ayebaishmo <ishmo255super@gmail.com>
2025-03-29 15:20:54 +0300

    green (wrote code to pass test)

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-const assert = require('chai').assert;
-const multiply = require('../multiply');
+import {assert} from 'chai'
+import multiply from '../multiply.js';
 
 describe('Multiply Function', () => {
   it('should return 1 when multiplying 1 and 1', () => {
