608a51e31257218da1072f4f5eb66676454cce12
alima <alima.ayikoru1@gmail.com>
2025-03-29 15:29:40 +0300

    Red (cycle2:failing test failed).

--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 import {assert} from 'chai'
 import multiply from '../multiply.js';
 
+// describe('Multiply Function', () => {
+//   it('should return 1 when multiplying 1 and 1', () => {
+//     assert.equal(multiply(1, 1), 1);
+//   });
+// });
+
 describe('Multiply Function', () => {
-  it('should return 1 when multiplying 1 and 1', () => {
-    assert.equal(multiply(1, 1), 1);
+  it('should return 4 when multiplying 2 and 2', () => {
+    assert.equal(multiply(2, 2), 4);
   });
 });
+
