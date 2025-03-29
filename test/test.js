1444fa29483237cd64d5255b8c6a1ccde5cb397c
alima <alima.ayikoru1@gmail.com>
2025-03-29 15:39:34 +0300

    Green (Cycle4: added another test.)

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 import {assert} from 'chai'
 import multiply from '../multiply.js';
 
-// describe('Multiply Function', () => {
-//   it('should return 1 when multiplying 1 and 1', () => {
-//     assert.equal(multiply(1, 1), 1);
-//   });
-// });
+describe('Multiply Function', () => {
+  it('should return 1 when multiplying 1 and 1', () => {
+    assert.equal(multiply(1, 1), 1);
+  });
+});
 
 describe('Multiply Function', () => {
   it('should return 4 when multiplying 2 and 2', () => {
@@ -13,3 +13,8 @@ describe('Multiply Function', () => {
   });
 });
 
+describe('Multiply Function', () => {
+  it('should return 16 when multiplying 4 and 4', () => {
+    assert.equal(multiply(4, 4), 16);
+  });
+});
