4821a7f06bb5f88d1f772999240f082f10a80b36
ayebaishmo <ishmo255super@gmail.com>
2025-04-01 12:23:15 +0300

    one describe function

--- test/test.js
+++ test/test.js
@@ -9,15 +9,12 @@ describe('Multiply Function', () => {
   it('should return 1 when multiplying 1 and 1', () => {
     assert.equal(multiply(1, 1), 1);
   });
-});
 
-describe('Multiply Function', () => {
   it('should return 4 when multiplying 2 and 2', () => {
     assert.equal(multiply(2, 2), 4);
   });
-});
 
-describe('Multiply Function', () => {
+
   it('should return 16 when multiplying 4 and 4', () => {
     assert.equal(multiply(4, 4), 16);
   });
